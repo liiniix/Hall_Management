e7c2c490a30570cbb241a4629ca4d9c02857a54a
liiniix <thaki240198cgc@gmail.com>
2020-02-18 20:00:47 +0600

    stu info

--- sqlal.py
+++ sqlal.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, redirect, request, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, PasswordField, SubmitField, ValidationError
+from wtforms import StringField, IntegerField, validators, PasswordField, SubmitField, ValidationError
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, current_user, login_user, login_required, logout_user
 
 app = Flask(__name__)
 Bootstrap(app)
 app.secret_key = 'abul'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/thaki/ABUL/Hall_Management/test.py'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/thaki/ABUL/Hall_Management/test.db'
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -19,24 +19,25 @@ login_manager.init_app(app)
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    reg = db.Column(db.String(15), primary_key=True)
     username = db.Column(db.String(80))
     email = db.Column(db.String(120))
+    session = db.Column(db.String(10))
+    dept = db.Column(db.String(30))
+    roll = db.Column(db.Integer)
+    address = db.Column(db.String(50))
+    serial = db.Column(db.Integer)
     password = db.Column(db.String(50))
 
-    def __init__(self, un, em, pas):
-        self.username = un
-        self.email = em
-        self.password = pas
 
     def __repr__(self):
-        return "<User(%s, %s, %s)>" % (self.username, self.email, self.password)
+        return "<User(%s, %s, %s)>" % (self.reg, self.email, self.password)
 
     def is_active(self):
         return True
 
     def get_id(self):
-        return str(self.id)
+        return str(self.reg)
 
     def is_authenticated(self):
         return True
@@ -47,40 +48,43 @@ class User(db.Model):
 
 @login_manager.user_loader
 def user_loader(user_id):
-    return User.query.get(int(user_id))
+    return User.query.get(user_id)
 
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[validators.DataRequired(),validators.Length(min=5,max=10)])
+    reg = StringField('REgistration No', validators=[validators.DataRequired()])
     password = PasswordField('Password', validators=[validators.DataRequired(),validators.Length(min=8)])
     submit = SubmitField('Submit')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+    def validate_reg(self, reg):
+        user = User.query.filter_by(reg=reg.data).first()
         if user is None:
             raise ValidationError('Please Register')
 
-
-
 class RegisterForm(FlaskForm):
+    reg = StringField('REgistration Number', [validators.DataRequired()])
     username = StringField('Username', validators=[validators.DataRequired(),validators.Length(min=5,max=10)])
     email = StringField('Email', validators=[validators.DataRequired(),validators.Length(min=5,max=50),validators.Email()])
+    session = StringField('Session', validators=[validators.DataRequired()])
+    dept = StringField('Department', validators=[validators.DataRequired()])
+    roll = IntegerField('Class Roll', validators=[validators.DataRequired()])
+    address = StringField('Address', validators=[validators.DataRequired()])
+    serial = IntegerField('Serial of Admission Test', validators=[validators.DataRequired()])
     password = PasswordField('Password', validators=[validators.DataRequired(),validators.Length(min=8)])
     submit = SubmitField('Submit')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+    def validate_reg(self, reg):
+        user = User.query.filter_by(reg=reg.data).first()
         if user is not None:
-            raise ValidationError('Please use a different username.')
-
+            raise ValidationError('Registered Already')
 def insert_user():
-    a = User(request.form['username'], request.form['email'], request.form['password'])
+    a = User(reg = request.form['reg'],username = request.form['username'], email = request.form['email'], password = request.form['password'],  session = request.form['session'], dept = request.form['dept'], roll = request.form['roll'], address = request.form['address'], serial = request.form['serial'])
     db.session.add(a)
     db.session.commit()
 
-def user_pass_matched():
-    user = User.query.filter_by(username=request.form['username']).first()
+def reg_pass_matched():
+    user = User.query.filter_by(reg=request.form['reg']).first()
     if user.password == request.form['password']:
         login_user(user, remember=True)
         return True
@@ -106,7 +110,7 @@ def login():
         return redirect('/verified')
     form = LoginForm()
     if request.method=='POST' and form.validate_on_submit():
-        if user_pass_matched():
+        if reg_pass_matched():
             flash('Login success')
             return redirect('/verified')
         else:
